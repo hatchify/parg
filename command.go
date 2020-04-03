fd4c47738174c949ddeeb91de904b76668403d89
Lytic <d.halman@gmail.com>
2020-04-02 23:33:13 -0700

    gomu: Deploy local changes before incrementing version from v0.1.14

--- command.go
+++ command.go
@@ -1,5 +1,11 @@
 package flag
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 // Command represents an allowed command in the structure:
 // `./<program> <optional -flags (single)> <command (single)> <optional arguments (allows multiple)> <optional -flags (allows multiple)>`
 // Lack of commands implies no commands are allowed
@@ -26,6 +32,79 @@ func NewCommand() (cmd *Command) {
 
 	cmd = &command
 	return
+}
+
+// Help will return all available commands and flags
+func Help() string {
+	msg := "\nCommands:\n"
+	for _, cmd := range staticParg.AllowedCommands {
+		if strings.TrimSpace(cmd.Action) != "" {
+			msg += fmt.Sprintf(" %s %s\n  :: ", os.Args[0], cmd.Action)
+		} else {
+			msg += " <no command>\n  :: "
+		}
+		msg += cmd.Help
+		msg += "\n\n"
+	}
+
+	msg += "Flags:\n"
+	for _, flag := range staticParg.GlobalFlags {
+		msg += fmt.Sprintf(" %s\n  :: %s\n\n", flag.Identifiers, flag.Help)
+	}
+
+	return msg
+}
+
+// ShowHelp will return a command's help. If help is the command, returns first arg or general help
+func (cmd *Command) ShowHelp() string {
+	msg := "\nCommand: "
+	if cmd.Action == "help" {
+		if len(cmd.Arguments) == 0 {
+			// Show regular help
+			if len(cmd.Flags) == 0 {
+				return Help()
+			}
+		} else {
+			for _, argCmd := range staticParg.AllowedCommands {
+				if name, ok := cmd.Arguments[0].Value.(string); ok {
+					if argCmd.Action == name {
+						// Show help for this cmd
+						cmd.Action = argCmd.Action
+						cmd.Help = argCmd.Help
+						break
+					}
+				}
+				if argCmd.Action == cmd.Arguments[0].Name {
+					// Show help for this cmd
+					cmd.Action = argCmd.Action
+					cmd.Help = argCmd.Help
+					break
+				}
+			}
+		}
+	}
+
+	if cmd.Action == "help" {
+		msg = ""
+	} else {
+		msg += cmd.Action + "\n"
+		if strings.TrimSpace(cmd.Action) != "" {
+			msg += fmt.Sprintf(" %s %s\n  :: ", os.Args[0], cmd.Action)
+		} else {
+			msg += " <no command>\n  :: "
+		}
+		msg += cmd.Help
+		msg += "\n"
+	}
+
+	if len(cmd.Flags) > 0 {
+		msg += "\nFlags:\n"
+		for _, flag := range cmd.Flags {
+			msg += fmt.Sprintf(" %s\n  :: %s\n\n", flag.Identifiers, flag.Help)
+		}
+	}
+
+	return msg
 }
 
 // Args returns array of arg names
